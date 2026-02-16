66f60f900a2a7e8ddb16152d432eedb8249b4a44
bzrak <67083456+bzrak@users.noreply.github.com>
2026-02-15 21:56:11 -0300

    fix error handling when authenticating a user, add default role when registering and eagerload role when getting a user from the database

--- backend/forum/auth/service.py
+++ backend/forum/auth/service.py
@@ -3,8 +3,10 @@ from datetime import datetime, timezone
 
 from argon2.exceptions import VerifyMismatchError
 from fastapi import Request
+from sqlalchemy import func
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 from sqlalchemy.sql import select
 from redis.asyncio import Redis
 
@@ -27,6 +29,7 @@ class AuthService:
         """Register a new User."""
         user = User(**user_in.model_dump(exclude={"password"}))
         user.set_password(user_in.password)
+        user.role_id = 1  # TODO: remove hardcode
 
         return await self._create(session, user)
 
@@ -42,14 +45,20 @@ class AuthService:
         try:
             user = await self._get_by_username(session, user_in.username)
             if not user:
-                verify_hash(user_in.password, DUMMY_HASH)
-                log.warning(
-                    f"Failed login attempt with non-existent username: {user_in.username}"
-                    f"from IP: {request.client.host} at {datetime.now(timezone.utc)}"  # type: ignore
-                )
                 raise IncorrectPasswordOrUsername
             user.verify_password(user_in.password)
             return user
+        except IncorrectPasswordOrUsername:
+            log.warning(
+                f"Failed login attempt with non-existent username: {user_in.username}"
+                f"from IP: {request.client.host} at {datetime.now(timezone.utc)}"  # type: ignore
+            )
+            try:
+                verify_hash(user_in.password, DUMMY_HASH)
+            except:  # noqa
+                pass
+            raise IncorrectPasswordOrUsername
+
         except VerifyMismatchError:
             log.warning(
                 f"Failed login attempt with wrong password for username: {user_in.username} "
@@ -71,6 +80,19 @@ class AuthService:
             raise InsufficientPermission
         except Exception as e:
             log.error(f"Unexpected error when checking authorization for {user}: {e}")
+            raise
+
+    async def list_users(self, session: AsyncSession) -> tuple[list[User], int]:
+        """List all users."""
+        try:
+            st = select(User)
+            count_st = select(func.count()).select_from(User)
+            res = await session.scalars(st)
+            total = await session.scalar(count_st) or 0
+
+            return (res.all(), total)  # type: ignore
+        except Exception as e:
+            log.error("Unexpected error during user listing: ", e)
             raise
 
     async def _get(self, session: AsyncSession, id: int) -> User | None:
@@ -95,7 +117,9 @@ class AuthService:
         self, session: AsyncSession, username: str
     ) -> User | None:
         """Returns a User by Username."""
-        res = await session.execute(select(User).where(User.username == username))
+        res = await session.execute(
+            select(User).where(User.username == username).options(joinedload(User.role))
+        )
         return res.scalar()
 
     async def _create(self, session: AsyncSession, user: User) -> User:
