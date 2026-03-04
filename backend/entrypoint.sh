725c42b68f4a1fd267dec78cf00b164d46430c79
berzrk1 <67083456+berzrk1@users.noreply.github.com>
2026-03-04 10:36:01 -0300

    feat: configure how to run uvicorn depending on the environment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,8 @@ set -e
 
 uv run alembic upgrade head
 
-exec uv run uvicorn forum.main:app --host 0.0.0.0 --port 8000 --reload
+if [ "$ENVIRONMENT" == "development" ]; then
+  exec uv run uvicorn forum.main:app --host 0.0.0.0 --port 8000 --reload
+else
+  exec uv run uvicorn forum.main:app --host 0.0.0.0 --port 8000 --workers 4
+fi
